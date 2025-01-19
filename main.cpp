0462cbc473b164c764b8362a8b7ce2a87c3f00ae
msemwa <msemwa@THADEI.thadei.com>
2025-01-19 16:34:50 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<GL/freeglut.h>
+/*?void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(-0.5, 0.5);
+    glVertex2f(0.5, 0.5);
+    glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
+}
+*/
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL Setup Test");
+    glutInitWindowSize(320, 320);
+    glutInitWindowPosition(50, 50);
+    //glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,1 @@
+g++ *.cpp -o main -lGL -lGLU -lglut && ./main
